219b5ed47b56a8695513ec10f1b5852db66d3174
navjot singh <navjot singh>
2017-09-15 01:40:45 +0530

    welcome spy

---
+++ main.py
@@ -0,0 +1,8 @@
+print "hello "
+print "let's get started "
+spyname=raw_input("what is ur name")
+#raw_input =this will convert user input into  a string
+print "welcome"+spyname
+salutation=raw_input("should i call you Mr.or Miss. ")
+print "Alright "+ salutation+spyname+" I would like to know more about you"
+
