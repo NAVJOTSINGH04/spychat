7a9775aa63db6cd979de28802f2c7425e5d72bbe
navjot singh <navjot singh>
2017-10-20 22:20:14 +0530

    finial submission

--- spy_details.py
+++ spy_details.py
@@ -1,7 +1,33 @@
-spy = {
-  'name': 'bond',
-  'salutation': 'Mr.',
-  'age': 24,
-  'rating': 4.7,
-  'is_online': True
-}
+from datetime import datetime
+# # spy = {
+#     'name' : 'Bond',
+#     'salu' : 'Dr.',
+#     'age' : 23,
+#     'rating' : 4.7,
+#     'is_online' : True
+# }
+class Chat:
+
+    def __init__(self, name, message, sent_by_me):
+        self.name = name
+        self.message = message
+        self.time = datetime.now().strftime("%b %d %Y %H:%M:%S")
+        self.sent_by_me = sent_by_me
+
+
+
+class Spy:
+
+       def __init__(self, name, salu, age, rating):
+        self.name = name
+        self.salu = salu
+        self.age = age
+        self.rating =rating
+        self.is_online = True
+        self.chats = []
+        self.current_status = None
+#
+# friends = []
+# spy = Spy('Bond', 'Mr.', 23, 5)
+# # friends.append(list(spy))
+# print list(spy)
